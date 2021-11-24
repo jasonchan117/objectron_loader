28cc40cb1b65dab2f24723ac5a26b15065afd0a3
Jasonchan_Lab <18933818702@163.com>
2021-11-23 22:09:16 -0500

    The dataset utils.

---
+++ dataset_utils.py
@@ -0,0 +1,170 @@
+import numpy as np
+import numpy as np
+import os
+import struct
+import sys
+import subprocess
+import cv2
+from objectron.schema import a_r_capture_metadata_pb2 as ar_metadata_protocol
+# The annotations are stored in protocol buffer format.
+from objectron.schema import object_pb2 as object_protocol
+from objectron.schema import annotation_data_pb2 as annotation_protocol
+import objectron.dataset.box as Box
+from IPython.core.display import display,HTML
+import matplotlib.pyplot as plt
+import cv2 as cv
+import torch
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+import argparse
+import time
+import random
+import numpy.ma as ma
+import copy
+import math
+import scipy.misc
+import scipy.io as scio
+import cv2
+import _pickle as cPickle
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+from AdaBins.infer import InferenceHelper
+
+def project_points(points, projection_matrix, view_matrix, width, height):
+    p_3d = np.concatenate((points, np.ones_like(points[:, :1])), axis=-1).T
+    p_3d_cam = np.matmul(view_matrix, p_3d)
+    p_2d_proj = np.matmul(projection_matrix, p_3d_cam)
+    # Project the points
+    p_2d_ndc = p_2d_proj[:-1, :] / p_2d_proj[-1, :]
+    p_2d_ndc = p_2d_ndc.T
+
+    # Convert the 2D Projected points from the normalized device coordinates to pixel values
+    x = p_2d_ndc[:, 1]
+    y = p_2d_ndc[:, 0]
+    pixels = np.copy(p_2d_ndc)
+    pixels[:, 0] = ((1 + x) * 0.5) * width
+    pixels[:, 1] = ((1 + y) * 0.5) * height
+    pixels = pixels.astype(int)
+    return pixels
+def grab_frame(video_file, frame_ids):
+  """Grab an image frame from the video file."""
+  frames = []
+  capture = cv2.VideoCapture(video_file)
+  # width = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  # height = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+  for i in frame_ids:
+      capture.set(cv2.CAP_PROP_POS_FRAMES,i)
+
+      if capture.isOpened():
+          ret, frame = capture.read()
+          if not ret:
+              # print('Can\'t receive frame (stream end?). Exiting ...')
+              capture.release()
+              return -1
+          frames.append(frame)
+  capture.release()
+  return frames
+  # frame_size = width * height * 3
+  #
+  # for frame_id in frame_ids:
+  #   frame_filter = r'select=\'eq(n\,{:d})\''.format(frame_id)
+  #   command = [
+  #       'ffmpeg', '-i', video_file, '-f', 'image2pipe', '-vf', frame_filter,
+  #       '-pix_fmt', 'rgb24', '-vcodec', 'rawvideo', '-vsync', 'vfr', '-', '-loglevel', 'error', '-hide_banner'
+  #   ]
+  #   pipe = subprocess.Popen(
+  #       command, stdout=subprocess.PIPE, bufsize = 151 * frame_size)
+  #   current_frame = np.frombuffer(
+  #       pipe.stdout.read(frame_size), dtype='uint8').reshape(width, height, 3)
+  #   pipe.stdout.flush()
+  #
+  #   frames.append(current_frame)
+  # return frames
+def get_frame_annotation(annotation_filename):
+    """Grab an annotated frame from the sequence."""
+    result = []
+    instances = []
+    with open(annotation_filename, 'rb') as pb:
+        sequence = annotation_protocol.Sequence()
+        sequence.ParseFromString(pb.read())
+
+        object_id = 0
+        object_rotations = []
+        object_translations = []
+        object_scale = []
+        num_keypoints_per_object = []
+        object_categories = []
+        annotation_types = []
+
+        # Object instances in the world coordinate system, These are stored per sequence,
+        # To get the per-frame version, grab the transformed keypoints from each frame_annotation
+        for obj in sequence.objects:
+            rotation = np.array(obj.rotation).reshape(3, 3)
+            translation = np.array(obj.translation)
+            scale = np.array(obj.scale)
+            points3d = np.array([[kp.x, kp.y, kp.z] for kp in obj.keypoints])
+            instances.append((rotation, translation, scale, points3d))
+
+        # Grab teh annotation results per frame
+        for data in sequence.frame_annotations:
+            # Get the camera for the current frame. We will use the camera to bring
+            # the object from the world coordinate to the current camera coordinate.
+            transform = np.array(data.camera.transform).reshape(4, 4)
+            view = np.array(data.camera.view_matrix).reshape(4, 4)
+            intrinsics = np.array(data.camera.intrinsics).reshape(3, 3)
+            projection = np.array(data.camera.projection_matrix).reshape(4, 4)
+
+            keypoint_size_list = []
+            object_keypoints_2d = []
+            object_keypoints_3d = []
+            for annotations in data.annotations:
+                num_keypoints = len(annotations.keypoints)
+                keypoint_size_list.append(num_keypoints)
+                for keypoint_id in range(num_keypoints):
+                    keypoint = annotations.keypoints[keypoint_id]
+                    object_keypoints_2d.append((keypoint.point_2d.x, keypoint.point_2d.y, keypoint.point_2d.depth))
+                    object_keypoints_3d.append((keypoint.point_3d.x, keypoint.point_3d.y, keypoint.point_3d.z))
+                num_keypoints_per_object.append(num_keypoints)
+                object_id += 1
+            result.append((object_keypoints_2d, object_keypoints_3d, keypoint_size_list, view, projection))
+
+    return result, instances
+def get_geometry_data(geometry_filename):
+    sequence_geometry = []
+    with open(geometry_filename, 'rb') as pb:
+        proto_buf = pb.read()
+
+        i = 0
+        frame_number = 0
+
+        while i < len(proto_buf):
+            # Read the first four Bytes in little endian '<' integers 'I' format
+            # indicating the length of the current message.
+            msg_len = struct.unpack('<I', proto_buf[i:i + 4])[0]
+            i += 4
+            message_buf = proto_buf[i:i + msg_len]
+            i += msg_len
+
+            frame_data = ar_metadata_protocol.ARFrame()
+            frame_data.ParseFromString(message_buf)
+
+
+            # 4x4 row-major matrix expressing position and orientation of the camera in world coordinate space.
+            transform = np.reshape(frame_data.camera.transform, (4, 4))
+            # 4x4 row-major transform matrix appropriate for rendering 3D content to match the image captured by the camera.
+            projection = np.reshape(frame_data.camera.projection_matrix , (4, 4))
+            # 4x4 row-major transform matrix appropriate for converting from world-space to camera space. Relativized for the captured_image orientation (i.e.UILandscapeOrientationRight).
+            view = np.reshape(frame_data.camera.view_matrix , (4, 4))
+            position = transform[:3, -1]
+            # Point cloud
+            current_points = [np.array([v.x, v.y, v.z])
+                              for v in frame_data.raw_feature_points.point]
+            current_points = np.array(current_points)
+
+            sequence_geometry.append((transform, projection, view, current_points))
+    return sequence_geometry
